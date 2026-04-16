57af88f433a6937e9639aa5dba5d6bfe9878ea2b
Mahfuzur <kirbyx90@gmail.com>
2026-04-15 22:56:54 -0400

    Lab 2 - All iterations: Simple Bank Account

---
+++ Mahfuzur Shohid IT 114-008 - Lab 02/submit/cnt4/SavingsAccount.java
@@ -0,0 +1,27 @@
+public class SavingsAccount extends BankAccount {
+    // DO NOT CHANGE ANY VARIABLE, METHOD, OR CLASS NAMES. THE AUTOGRADER DEPENDS ON THEM.
+    public SavingsAccount(String accountHolder, double initialDeposit) {
+        // TODO: Call the parent constructor using 'super'
+
+        super(accountHolder, initialDeposit); //We can do a similar thing we did for the checking account for the savings
+
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        // TODO: Implement Savings withdrawal logic
+
+        if(amount > getBalance()){
+            
+            System.out.println("Transaction Denied: Insufficient funds"); //So since this is a savings accoount do not allow for an overdraft fee, we dont have to immplment it.
+        
+        } else {
+
+            setBalance(getBalance() - amount); //Its either they have the money or not. 
+ 
+        }
+
+        System.out.println("New Balance: $" + getBalance());
+
+    }
+}
